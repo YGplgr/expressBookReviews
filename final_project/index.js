1efed3e9ff760c21abfde2b1c8690c64ae32c231
YGplgr <ygofficepc@gmail.com>
2024-11-17 09:42:14 -0500

    commit3

--- final_project/index.js
+++ final_project/index.js
@@ -11,19 +11,19 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-    const token = req.session.token;
-    if (!token){
-        return res.status(401).json({messege: "Access denied. Token missing... "});
-    }
-    try {
-        const dCoded = jwt.verify(token, "jwt_secret_key");
-        req.user = dCoded;
-        next();        
-    }
-    catch (err){
-        res.status(403).json({error: "invalide token"});
-    }
-
+  if (req.session.authorization) { 
+    token = req.session.authorization['accessToken']; 
+    jwt.verify(token, "access", (err, user) => { 
+      if (!err) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(403).json({ message: "User not authenticated" });
+      }
+    });
+  } else {
+    return res.status(403).json({ message: "User not logged in" });
+  }
 });
  
 const PORT =5000;
