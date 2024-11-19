20f05821cdc0283e16a5cadb2f5520ecda91bbd5
YGplgry <ygofficepc@gmail.com>
2024-11-19 06:17:06 -0500

    9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -47,7 +47,7 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   }
 
   if (!books[isbn]) {
-    return res.status(404).json({ message: "ISBN ${isbn} not found" });
+    return res.status(404).json({ message: `ISBN ${isbn} not found` });
   }
 
   if (books[isbn].reviews[username]) {
@@ -64,8 +64,21 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 
 regd_users.delete("/auth/review/:isbn", (req, res) => {
     const isbn = req.params.isbn;
-    const review = req.query.review; 
     const username = req.session.username;
+
+    if (!username ) {
+    return res.status(400).json({ message: "Review or username cannot be empty" });
+  }
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: `ISBN ${isbn} not found` });
+  }
+
+  if(!books[isbn].reviews[username]){
+    return res.status(400).json({ message: `No reviews in book ${isbn} for this user` });
+  }
+  delete books[isbn].reviews[username];
+  return res.status(200).json({message: "Review deleted successfully"});
 
 });
 
