ddd0da2582c5dda5c0774d49e3f14964b55604fd
YGplgry <ygofficepc@gmail.com>
2024-11-19 05:44:54 -0500

    8.2

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -21,14 +21,16 @@ regd_users.post("/login", (req,res) => {
     const {username, password} = req.body;
     if (authenticatedUser && isValid) {
         // Generate JWT access token
-        const accessToken = jwt.sign({
+        const token = jwt.sign({
             data: password
-        }, 'access', { expiresIn: 60 * 60 });
+        }, 'your_jwt_secret_key', { expiresIn: 60 * 60 });
 
         // Store access token and username in session
-        req.session.accessToken = accessToken;
+        res.setHeader("Authorization", `Bearer ${token}`);
+        req.session.username = username;
+        req.session.token = token;
 
-        return res.status(200).send("User successfully logged in");
+        return res.json({message: "Login successful."});
     } else {
         return res.status(208).json({ message: "Invalid Login. Check username and password" });
     }
@@ -57,6 +59,13 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     return res.status(201).json({ message: "Review added successfully", reviews: books[isbn].reviews });
   }
 
+
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const review = req.query.review; 
+    const username = req.session.username;
 
 });
 
